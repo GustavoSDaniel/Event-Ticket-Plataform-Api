3eb3f46b7ba5eb7d20336445de7f6d12690075fe
GustavoSDaniel <gustavosdaniel@hotmail.com>
2025-07-19 17:42:06 -0300

    feat: criando Excecoes globais

---
+++ src/main/java/com/gustavosdaniel/tickets/common/ErrorDTO.java
@@ -0,0 +1,13 @@
+package com.gustavosdaniel.tickets.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDTO {
+
+    private String error;
+}
---
+++ src/main/java/com/gustavosdaniel/tickets/common/GlobalExceptionHandler.java
@@ -0,0 +1,89 @@
+package com.gustavosdaniel.tickets.common;
+
+import com.gustavosdaniel.tickets.user.UserNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorDTO> handleUserNotFoundException(UserNotFoundException e) {
+
+        log.error("Caught UserNotFoundException", e);
+
+        ErrorDTO errorDTO = new ErrorDTO();
+
+        errorDTO.setError("User not found");
+
+        return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    //Trata erros de validação de argumentos em métodos
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorDTO> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex
+    ){
+
+        log.error("Caught ConstraintViolationException", ex);
+
+        ErrorDTO errorDTO = new ErrorDTO();
+
+        BindingResult bindingResult = ex.getBindingResult();
+        List<FieldError> fieldErrorList = bindingResult.getFieldErrors();
+        String errorMessage = fieldErrorList.stream()
+                .findFirst()
+                .map(fieldError ->
+                        fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .orElse("Validation Error occurred");
+
+        errorDTO.setError(errorMessage);
+
+        return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    // quando as validações definidas nas anotações como @NotNull, @Size, @Pattern, etc., falham
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorDTO> handleConstraintViolation(ConstraintViolationException exception) {
+
+        log.error("Caught ConstraintViolationException", exception);
+
+        ErrorDTO errorDTO = new ErrorDTO();
+
+        String errorMessage =  exception.getConstraintViolations()
+                .stream()
+                .findFirst()
+                .map(violation ->
+                        violation.getPropertyPath() + ": " + violation.getMessage()
+                ).orElse("Constraint violation exception");
+
+        errorDTO.setError(errorMessage);
+
+        return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
+    }
+
+
+    //  Captura qualquer exceção não tratada pelos outros handlers
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDTO> handleException(Exception exception) {
+
+        log.error("Caught exception", exception);
+
+        ErrorDTO errorDTO = new ErrorDTO();
+
+        errorDTO.setError("An unknown error occurred");
+
+        return new ResponseEntity<>(errorDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
